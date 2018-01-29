0d32d0a82040aeba9900a79f94aa77d339472eb4
Skipper af Rosenborg <rosenborg.skipper@gmail.com>
2018-01-29 11:05:42 +0100

    Test to see if Esbens function

--- ML/AALR.R
+++ ML/AALR.R
@@ -1,1 +1,3 @@
 # This is Skipper af Rosenborg's and Esben Jørgensen Bager's implementation of "An Algorithmic Approach to Linear Regression" by Dimitris Bertsimas, Angela King
+
+# Kan du se at jeg har ændret noget nu?
