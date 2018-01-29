9a44bcb0e51ddb6a7e9098a083d504bc86e6d173
Skipper af Rosenborg <rosenborg.skipper@gmail.com>
2018-01-29 12:30:08 +0100

    Implemented Dataload

--- ML/AALR.R
+++ ML/AALR.R
@@ -1,3 +1,22 @@
-# This is Skipper af Rosenborg's and Esben Jørgensen Bager's implementation of "An Algorithmic Approach to Linear Regression" by Dimitris Bertsimas, Angela King
+# This is Skipper af Rosenborg's and Esben Jørgensen Bager's implementation of 
+#"An Algorithmic Approach to Linear Regression" by Dimitris Bertsimas, Angela King
 
-# Kan du se at jeg har ændret noget nu?
+# Clear environment
+rm(list=ls())
+
+# Load Data, missing values are noted with -99.99 or -999 and replaced with "NA"
+library(readr)
+#From 1927-2016
+AnnAverageEqualWeightedReturns     <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Annual - Average Equal Weighted Returns.csv", skip = 1, na = c("-99.99", "-999","NA"))
+AnnAverageValueWeightedReturns     <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Annual - Average Value Weighted Returns.csv", skip = 1, na = c("-99.99", "-999","NA"))
+
+#From 1926-2017
+AnnSumOfBESumOfMERatio              <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Annual - Sum of BE : Sum of ME.csv", skip = 1, na = c("-99.99", "-999","NA"))
+AnnValueWeightedAverageOfBEMERatio <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Annual - Value-Weighted Average of BE:ME.csv", skip = 1, na = c("-99.99", "-999","NA"))
+
+#From July 1926 - October 2017
+MonAverageEqualWeightedReturns    <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Monthly - Average Equal Weighted Returns.csv", skip = 1, na = c("-99.99", "-999","NA"))
+MonAverageValueWeightedReturns    <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Monthly - Average Value Weighted Returns.csv", skip = 1, na = c("-99.99", "-999","NA"))
+MonNumberOfFirmsInPortfolios.csv  <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Monthly - Number of Firms in Portfolios.csv", skip = 1, na = c("-99.99", "-999","NA"))
+MonAverageFirmSize                <- read_csv("~/Google Drive/DTU/10. Semester/Thesis/GitHubCode/Thesis/Data/Monthly - Average Firm Size.csv", skip = 1, na = c("-99.99", "-999","NA"))
+
